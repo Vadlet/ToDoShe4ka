9dd8ce3269c4e00343d2cfebfeec6f1f4fca9958
Vadlet <letuchyvadim95@yandex.ru>
2022-07-31 15:22:41 +0300

    Core Data

--- ToDoShe4ka/TaskListViewController.swift
+++ ToDoShe4ka/TaskListViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import CoreData
+
+protocol TaskViewControllerDelegate {
+    func reloadData()
+}
 
 class TaskListViewController: UITableViewController {
 
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let cellID = "taskID"
+    private var taskList: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         setupNavigationBar()
+        fetchData()
     }
     
     private func setupNavigationBar() {
@@ -40,6 +51,41 @@ class TaskListViewController: UITableViewController {
     
     @objc private func addNewTask() {
         let taskVC = TaskViewController()
+        taskVC.delegate = self
         present(taskVC, animated: true)
+    }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            taskList = try context.fetch(fetchRequest)
+        } catch let error {
+            print("Failed to fetch data" , error)
+        }
+    }
+}
+
+
+//MARK: - UITableViewDataSource
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+//MARK: - TaskViewControllerDelegate
+extension TaskListViewController: TaskViewControllerDelegate {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
     }
 }
--- ToDoShe4ka/TaskViewController.swift
+++ ToDoShe4ka/TaskViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskViewController: UIViewController {
     
-    private lazy var textField: UITextField = {
+    var delegate: TaskViewControllerDelegate?
+
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private lazy var newTaskTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Новая задача"
         textField.borderStyle = .roundedRect
@@ -30,7 +35,7 @@ class TaskViewController: UIViewController {
     private lazy var cancelButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor(named: "Red")
-        button.setTitle("Сохранить задачу", for: .normal)
+        button.setTitle("Назад", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.setTitleColor(UIColor.white , for: .normal)
         button.layer.cornerRadius = 4
@@ -38,11 +43,10 @@ class TaskViewController: UIViewController {
         return button
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setupSubviews(textField, saveButton, cancelButton)
+        setupSubviews(newTaskTextField, saveButton, cancelButton)
         setConstraints()
     }
     
@@ -53,26 +57,38 @@ class TaskViewController: UIViewController {
     }
     
     private func setConstraints() {
-        textField.translatesAutoresizingMaskIntoConstraints = false
+        newTaskTextField.translatesAutoresizingMaskIntoConstraints = false
         saveButton.translatesAutoresizingMaskIntoConstraints = false
         cancelButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
-            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            newTaskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            newTaskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            newTaskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
             
-            saveButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
-            saveButton.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
-            saveButton.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+            saveButton.topAnchor.constraint(equalTo: newTaskTextField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: newTaskTextField.leadingAnchor),
+            saveButton.trailingAnchor.constraint(equalTo: newTaskTextField.trailingAnchor),
             
             cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 20),
-            cancelButton.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
-            cancelButton.trailingAnchor.constraint(equalTo: textField.trailingAnchor)
+            cancelButton.leadingAnchor.constraint(equalTo: newTaskTextField.leadingAnchor),
+            cancelButton.trailingAnchor.constraint(equalTo: newTaskTextField.trailingAnchor)
         ])
     }
     
     @objc private func save() {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        task.title = newTaskTextField.text
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error)
+            }
+        }
+        delegate?.reloadData()
         dismiss(animated: true)
     }
     
